0b341ec101099bfe9dd1f7175245a5697db2da39
cfl <1243455143@qq.com>
2018-12-24 17:39:33 +0800

    多文件拖拽上传SFTP功能

---
+++ UpDiaryTool/UpDiaryTool/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace UpDiaryTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.showarea = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.filename = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // showarea
+            // 
+            this.showarea.AllowDrop = true;
+            this.showarea.Location = new System.Drawing.Point(12, 38);
+            this.showarea.Multiline = true;
+            this.showarea.Name = "showarea";
+            this.showarea.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.showarea.Size = new System.Drawing.Size(484, 196);
+            this.showarea.TabIndex = 0;
+            this.showarea.DragDrop += new System.Windows.Forms.DragEventHandler(this.show_DragDrop);
+            this.showarea.DragEnter += new System.Windows.Forms.DragEventHandler(this.up_DragEnter);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.filename});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 240);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(484, 70);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.Visible = false;
+            // 
+            // filename
+            // 
+            this.filename.HeaderText = "文件名";
+            this.filename.Name = "filename";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 333);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.showarea);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox showarea;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn filename;
+    }
+}
+
---
+++ UpDiaryTool/UpDiaryTool/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UpDiaryTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SFTPHelper sftp = null;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sftp = new SFTPHelper("192.168.7.23", "22", "Admin", "123");
+            showfile(sftp);
+
+        }
+        /// <summary>
+        /// 拖动到控件区域
+        /// </summary>
+        private void show_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Link;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+
+        }
+        /// <summary>
+        /// 拖动到控件区域并松开鼠标
+        /// </summary>
+        private void up_DragEnter(object sender, DragEventArgs e)
+        {
+
+            string [] localpathlist = (e.Data.GetData(DataFormats.FileDrop) as string []);
+            try
+            {
+                localpathlist.ToList().ForEach(n=> {
+                    sftp.Put(n, "/biz/371400/BizMsg/" + Path.GetFileName(n));
+                    showarea.Text = null;
+                    showfile(sftp);
+                });
+                
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+           
+            
+        }
+
+        public  void showfile(SFTPHelper sftp)
+        {
+            
+            ArrayList list = sftp.GetFileList("/biz/371400/BizMsg/", "");
+            //List<string> ls = new List<string>();
+            //foreach (var item in list)
+            //{
+            //    ls.Add(item.ToString());
+            //}
+            //指定数据源
+           // DataTable dt = ListToDatatableHelper.ToDataTable(ls);
+           // this.dataGridView1.DataSource = dt;
+            foreach (var item in list)
+            {
+                this.showarea.Text += item + "\r\n";
+                
+            }
+            //设置文字默认是不被选中状态
+            this.showarea.SelectionStart = 0;
+            this.showarea.SelectionLength = 0;
+        }
+    }
+}
